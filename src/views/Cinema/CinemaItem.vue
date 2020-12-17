efd530c868c20d371c6d1d294f03a9f5d4c5f898
631593796 <631593796@qq.com>
2020-12-17 17:22:16 +0800

    第二次提提交

---
+++ src/views/Cinema/CinemaItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>
+    <h4>{{data.name}}</h4>
+    <p>{{data.address}}</p>
+  </li>
+</template>
+<script>
+export default {
+  props: ['data'],
+  mounted () {
+    console.log(this.data)
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+    li{
+      padding:10px;
+      width:70%;
+    }
+</style>
